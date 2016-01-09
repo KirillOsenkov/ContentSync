769b241043bff96febd9879f123630ccb9d05662
Kirill Osenkov <github@osenkov.com>
2016-01-08 22:57:55 -0800

    Initial commit.

---
+++ src/ContentSync/ContentSync.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+namespace GuiLabs.FileUtilities
+{
+    internal class ContentSync
+    {
+        private static void Main(string[] args)
+        {
+            string source = null;
+            string destination = null;
+            if (args.Length == 2)
+            {
+                source = args[0];
+                destination = args[1];
+            }
+            else
+            {
+                PrintUsage();
+                return;
+            }
+
+            if (Directory.Exists(source))
+            {
+                source = Path.GetFullPath(source);
+                destination = Path.GetFullPath(destination);
+                Sync.Directories(source, destination);
+                return;
+            }
+
+            if (File.Exists(source) && Directory.Exists(destination))
+            {
+                Sync.Files(source, Path.Combine(destination, Path.GetFileName(source)));
+                return;
+            }
+
+            if (File.Exists(source))
+            {
+                Sync.Files(source, destination);
+                return;
+            }
+
+            Console.Error.WriteLine($"Cannot sync {source} to {destination}");
+        }
+
+        private static void PrintUsage()
+        {
+            Console.WriteLine(@"Usage: ContentSync.exe <source> <destination>
+    Copy/mirror/sync the destination folder to look exactly like source folder.
+    Copies missing files, deletes files from destination that are not in source,
+    overwrites files in destination that have different contents than in source.
+    Doesn't take into account file and date timestamps, works off content only.");
+        }
+    }
+}
---
+++ src/ContentSync/Sync.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GuiLabs.FileUtilities
+{
+    public class Sync
+    {
+        public static void Directories(string source, string destination)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Files(string source, string destination)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
